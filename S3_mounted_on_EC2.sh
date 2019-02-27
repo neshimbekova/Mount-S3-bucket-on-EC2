f58d088bf1acde4df632bc0544261fda850844e6
alibek17 <47870220+alibek17@users.noreply.github.com>
2019-02-27 01:27:46 -0600

    Update S3_mounted_on_EC2.sh
    We need a bucket name and iam role from a user, but I don't think that the user has to specify the name of a folder, that is going to be used as a mount point. So I've changed it to /images, we are going to need this folder in a php script too.
    
    Also installed s3fs form Installation from pre-built packages from epel repo instead of compiling and installing.

--- S3_mounted_on_EC2.sh
+++ S3_mounted_on_EC2.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
-#This script works on AWS on Amazon Linux 2 AMI
-#This script installs Apache and mountss your existing 'ec2' to 's3'bucket
+#This script works on AWS on Centos 7
+#This script installs Apache and mounts your existing amazon s3 bucket to ec2 instance
 
 #Installing Apache, starting and making it boot persistant
 yum install httpd -y
 systemctl start httpd
-systemctl enable httpdg 
+systemctl enable httpd 
 
-#Install the dependencies
-yum -y install automake fuse fuse-devel gcc-c++ git libcurl-devel libxml2-devel make openssl-devel -y
-#Clone s3fs source code from github
-git clone https://github.com/s3fs-fuse/s3fs-fuse.git
-#Next 4 lines will change to source code  directory, compile and install the code 
-cd s3fs-fuse/
-./autogen.sh && ./configure
-make
-make install
+#Install the s3fs
+yum install epel-release -y
+yum install s3fs-fuse -y
 
 #create a directory and mount S3bucket in it.
-read -p "Enter your folder name :" folder
-mkdir -p /var/www/html/$folder
 read -p "Enter your s3 bucket name :" bucket
 read -p "Enter your IAM role name :" iamrole
-s3fs $bucket /var/www/html/$folder -o iam_role=$iamrole
-s3fs -o allow_other,iam_role=$iamrole,nonempty,uid=48,gid=48,umask=777,use_cache=/root/cache $bucket /var/www/html/$folder
+s3fs $bucket /var/www/html/images -o iam_role=$iamrole
+s3fs -o allow_other,iam_role=$iamrole,nonempty,uid=48,gid=48,umask=777,use_cache=/root/cache $bucket /var/www/html/images
